d543ed1fad4cd9c4fd92810c782a30669cf05868
Mauro <mpino1701@gmail.com>
2023-09-30 21:43:26 -0300

    add multiple database

--- db/connections.go
+++ db/connections.go
@@ -1,14 +1,21 @@
 package db
 
 import (
+	"errors"
+	"fmt"
+	"log"
+
+	conf "github.com/Mau005/MyExpenses/configuration"
 	"github.com/Mau005/MyExpenses/models"
+	"gorm.io/driver/mysql"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 var DB *gorm.DB
 
-func GenerateDefaultAttribute() {
+func GenerateCategory() {
 	DB.Create(&models.Category{
 		Model:       gorm.Model{ID: 1},
 		Name:        "Supermercados",
@@ -136,15 +143,57 @@ func GenerateDefaultAttribute() {
 	})
 }
 
-func ConnectionSqlite() error {
+func connectionSqlite(database conf.DataBase, debugMode bool) error {
 	var err error
 
-	DB, err = gorm.Open(sqlite.Open("MyExpenses.db"), &gorm.Config{})
+	logDebug := logger.Silent
+	if debugMode {
+		logDebug = logger.Warn
+	}
+
+	DB, err = gorm.Open(sqlite.Open(database.SqlitePath), &gorm.Config{
+		Logger: logger.Default.LogMode(logDebug),
+	})
 	if err != nil {
 		return err
 	}
 	AutoMigrate()
+	log.Println(conf.CONNECTION_SQLITE)
 	return nil
+
+}
+
+func connectionMysql(database conf.DataBase, debugMode bool) error {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		database.User, database.Password, database.Host, database.Port, database.NameDB)
+
+	logDebug := logger.Silent
+	if debugMode {
+		logDebug = logger.Warn
+	}
+	var err error
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logDebug),
+	})
+	if err != nil {
+		return err
+	}
+	AutoMigrate()
+	log.Println(conf.CONNECTION_MYSQL)
+	return nil
+}
+
+func ConnectionDataBase() error {
+	switch conf.Config.DataBase.Engine {
+	case "sqlite":
+		return connectionSqlite(conf.Config.DataBase, conf.Config.Server.Debug)
+
+	case "mysql":
+		return connectionMysql(conf.Config.DataBase, conf.Config.Server.Debug)
+
+	default:
+		return errors.New(conf.ERROR_DATABASE_GET)
+	}
 
 }
 
