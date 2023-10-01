79dc106bb8b670f1dbb66426ba67ddb8a4e4cebd
Mauro <mpino1701@gmail.com>
2023-09-30 21:42:07 -0300

    add const msg error

--- configuration/costant.go
+++ configuration/costant.go
@@ -6,6 +6,13 @@ const (
 )
 
 const (
+	SECURITY_DEBUG    = "security debug mode"
+	SECURITY_NORMAL   = "security debug normal"
+	CONNECTION_MYSQL  = "You have successfully connected to the Mysql database"
+	CONNECTION_SQLITE = "You have successfully connected to the Sqlite database"
+)
+
+const (
 	EXPIRATION_TOKEN = 24 //HOURS
 	NAME_SESSION     = "Authorization"
 	DELETE_USER      = "Se ha eliminado el usuario"
@@ -17,6 +24,8 @@ const (
 const (
 	ERROR_EMPTY_FIELD       = "Se debe ingresar datos"
 	ERROR_INDEX             = "Atributo no indexado para procesar"
+	ERROR_DATABASE_GET      = "No se ha encontrado ningun motor de basededatos para procesar"
+	ERROR_SERVICE_SECURITY  = "Service error in Security"
 	ERROR_SERVICE_USER      = "Service error in user"
 	ERROR_SERVICE_CATEGORY  = "Service error in Category"
 	ERROR_SERVICE_EXPENSES  = "Service error in Expenses"
